c5334f389b44d63211ea477b33a3c40cc5905903
guilherssousa <guilherssousa@gmail.com>
2022-03-20 20:07:31 -0300

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
+
+// module.exports = withBundleAnalyzer({
+//   webpack: function (config) {
+//     config.module.rules.push({ test: /\.md$/, use: "raw-loader" });
+
+//     return config;
+//   },
+// });
+
 module.exports = {
   webpack: function (config) {
     config.module.rules.push({ test: /\.md$/, use: "raw-loader" });
