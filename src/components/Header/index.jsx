be18b8c5491ca66d6812af8d1ec90f1d010a5e9f
guilherssousa <guilherssousa@gmail.com>
2022-03-20 02:08:14 -0300

    Button contrast

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -56,11 +56,14 @@ function Header() {
 
           <Stack mt="4" direction={{ base: "column", md: "row" }}>
             <Button
-              colorScheme="blue"
+              bg="blue.700"
               size="lg"
               fontSize={20}
               p={[4, 6, 8]}
               w={{ sm: "80vw", md: 40 }}
+              _hover={{
+                bg: "blue.600",
+              }}
             >
               Ver os Hooks
             </Button>
